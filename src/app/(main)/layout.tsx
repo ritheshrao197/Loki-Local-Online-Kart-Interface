7767c99f0b3b986e0999895c2eba5b242c4422d0
Rithesh Rao <rithesh197rao@gmail.com>
2025-09-26 02:58:03 +0000

    admin and seller should able to see the homepage

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import { Footer } from "@/components/layout/Footer";
 import { Header } from "@/components/layout/Header";
 import { useIsMobile } from "@/hooks/use-mobile";
@@ -13,19 +13,23 @@ export default function MainLayout({
 }) {
   const isMobile = useIsMobile();
   const router = useRouter();
+  const pathname = usePathname();
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
       const userRole = sessionStorage.getItem('userRole');
       const userId = sessionStorage.getItem('userId');
 
-      if (userRole === 'admin') {
-        router.replace('/admin');
-      } else if (userRole === 'seller' && userId) {
-        router.replace(`/dashboard/${userId}`);
+      // Only redirect if they are NOT on the homepage
+      if (pathname !== '/') {
+        if (userRole === 'admin') {
+          router.replace('/admin');
+        } else if (userRole === 'seller' && userId) {
+          router.replace(`/dashboard/${userId}`);
+        }
       }
     }
-  }, [router]);
+  }, [router, pathname]);
 
   return (
     <div className="flex flex-col min-h-screen">
